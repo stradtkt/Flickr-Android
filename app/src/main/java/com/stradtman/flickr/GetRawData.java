6c0befa045a840ad6592369c1bf344a1eeef3a35
Kevin <stradtkt22@gmail.com>
2019-02-27 16:50:28 -0500

    Added items into main activity and get raw data

--- app/src/main/java/com/stradtman/flickr/GetRawData.java
+++ app/src/main/java/com/stradtman/flickr/GetRawData.java
@@ -14,14 +14,24 @@ enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK}
 class GetRawData extends AsyncTask<String, Void, String> {
     private static final String TAG = "GetRawData";
     private DownloadStatus mDownloadStatus;
+    private final OnDownloadComplete mCallback;
 
-    public GetRawData() {
+    interface OnDownloadComplete {
+        void onDownloadComplete(String data, DownloadStatus status);
+    }
+
+    public GetRawData(OnDownloadComplete callback) {
         mDownloadStatus = DownloadStatus.IDLE;
+        mCallback = callback;
     }
 
     @Override
     protected void onPostExecute(String s) {
-        super.onPostExecute(s);
+        Log.d(TAG, "onPostExecute: parameter = " + s);
+        if(mCallback != null) {
+            mCallback.onDownloadComplete(s, mDownloadStatus);
+        }
+        Log.d(TAG, "onPostExecute: ends");
     }
 
     @Override
@@ -42,13 +52,32 @@ class GetRawData extends AsyncTask<String, Void, String> {
             Log.d(TAG, "doInBackground: The response code was " + response);
             StringBuilder result = new StringBuilder();
             reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while(null != (line = reader.readLine())) {
+                result.append(line).append("\n");
+            }
+            mDownloadStatus = DownloadStatus.OK;
+            return result.toString();
+
         } catch(MalformedURLException e) {
             Log.e(TAG, "doInBackground: Invalid URL " + e.getMessage());
         } catch(IOException e) {
             Log.e(TAG, "doInBackground: IO Exception reading data: " + e.getMessage());
         } catch(SecurityException e) {
             Log.e(TAG, "doInBackground: Security Exception. Needs permission? " + e.getMessage());
+        } finally {
+            if(connection != null) {
+                connection.disconnect();
+            }
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch(IOException e) {
+                    Log.e(TAG, "doInBackground: Error closing stream " + e.getMessage());
+                }
+            }
         }
+        mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
         return null;
     }
 }
--- app/src/main/java/com/stradtman/flickr/MainActivity.java
+++ app/src/main/java/com/stradtman/flickr/MainActivity.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GetRawData.OnDownloadComplete {
 
     private static final String TAG = "MainActivity";
 
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,nougat,sdk&tagmode=any&format=json&nojsoncallback=1");
         Log.d(TAG, "onCreate: ends");
     }
 
@@ -41,5 +43,13 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.d(TAG, "onOptionsItemSelected() returned: " + true);
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        if(status == DownloadStatus.OK) {
+            Log.d(TAG, "onDownloadComplete: data is " + data);
+        } else {
+            Log.e(TAG, "onDownloadComplete: failed with status " + status);
+        }
     }
 }
